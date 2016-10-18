afaca7cb09e7817ddf747717416096acc253c760
Ahmad Awais <mrahmadawais@gmail.com>
2016-10-18 09:00:21 +0500

    Better Documentation 💯

--- revision-browser.js
+++ revision-browser.js
@@ -1,4 +1,12 @@
-jQuery( document ).ready( function($){
+/**
+ * Revision Browser JS
+ *
+ * JS responsible for frontend revision browsing.
+ *
+ * @since   0.1.0
+ * @package RBR
+ */
+jQuery( document ).ready( function($) {
    var $content = $( '.' + REVBROWSER.content );
    var $title = $( '.' + REVBROWSER.title );
    var $nav = $( '#revision-browser-nav' );
@@ -7,35 +15,35 @@ jQuery( document ).ready( function($){
    var revisions, revision, count;
    var current = 0;
 
-   $.get( REVBROWSER.api ).success( function( r ){
+   $.get( REVBROWSER.api ).success( function( r ) {
       revisions = r;
       count = r.length;
 
-   }).error( function( r ){
+   }).error( function( r ) {
       console.log( r.responseText );
       $nav.hide().css( 'visibility', 'hidden' ).attr( 'aria-hidden', true );
    });
 
-   $( '#revision-browser-next' ).on( 'click', function(e){
+   $( '#revision-browser-next' ).on( 'click', function(e) {
       e.preventDefault();
       current = current - 1;
-      if( current > -1 ){
+      if ( current > -1 ) {
          revision = revisions[ current ];
          placeRevision( revision );
       }
    });
 
-   $( '#revision-browser-prev' ).on( 'click', function(e){
+   $( '#revision-browser-prev' ).on( 'click', function(e) {
       e.preventDefault();
       current = current + 1;
-      if( current < count ){
+      if ( current < count ) {
          revision = revisions[ current ];
          placeRevision( revision );
       }
 
    });
 
-   function placeRevision( revision ){
+   function placeRevision( revision ) {
     console.log( revision )
       $title.html( revision.title.rendered );
       $content.html( revision.content.rendered );
