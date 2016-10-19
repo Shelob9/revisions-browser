c24cfd8723b51a445e4d8a8081ddc3aad55927bc
Evan Mullins <evanrm@gmail.com>
2016-10-19 13:07:13 -0400

    only add if there are revisions
    add check to only add the revision browser if there are in fact revisions to browse.

--- revision-browser.php
+++ revision-browser.php
@@ -28,6 +28,11 @@ function admin_bar_revisions_browser() {
 	if ( ! is_single() ) {
 		return;
 	}
+	
+	// Only add if current post has revisions
+	if ( count( wp_get_post_revisions( $post->ID ) ) <= 1 ) {
+		return;
+	}
 
 	add_action( 'admin_bar_menu', 'admin_bar_revisions_browser_toolbar', 999 );
 
